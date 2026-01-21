540e230db51b851b5987dcddb371e23dbf7d05da
Tj-Alder-ANU <u7287129@anu.edu.au>
2026-01-20 19:48:59 -1000

    Fix incorrect scrambler logic. Replace integer division with mod

--- projects/cryptography/cryptography/enigma/enigma.py
+++ projects/cryptography/cryptography/enigma/enigma.py
@@ -9,13 +9,13 @@ class Enigma:
     def __init__(self, alphabet: Optional[List[str]]=None, seed: Optional[int]=None):
         alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] if alphabet is None else alphabet
         self.p = Plugboard(alphabet=alphabet, seed=seed)
-        self.s1 = Scrambler(divisor=1, alphabet=alphabet, seed=seed)
-        self.s2 = Scrambler(divisor=len(alphabet), alphabet=alphabet, seed=seed)
-        self.s3 = Scrambler(divisor=len(alphabet)**2, alphabet=alphabet, seed=seed)
+        self.s1 = Scrambler(mod=1, alphabet=alphabet, seed=seed)
+        self.s2 = Scrambler(mod=len(alphabet), alphabet=alphabet, seed=seed)
+        self.s3 = Scrambler(mod=len(alphabet)**2, alphabet=alphabet, seed=seed)
     
     def __call__(self, m: str):
         m = m.replace(" ", "")
-        for p_in, p_out in self.p:
+        for p_in, p_out in self.p: # Fix this
             c0 = m.replace(p_in, p_out)
         c1 = self.s1(c0)
         c2 = self.s2(c1)
--- projects/cryptography/cryptography/enigma/scrambler.py
+++ projects/cryptography/cryptography/enigma/scrambler.py
@@ -5,24 +5,29 @@ from collections import deque
 
 class Scrambler:
     
-    def __init__(self, divisor: int=1, alphabet: Optional[List[str]]=None, seed: Optional[int]=None):
+    def __init__(self, mod: int=1, alphabet: Optional[List[str]]=None, seed: Optional[int]=None):
         rng = random.Random(seed) if seed is not None else random.Random()
         alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"] if alphabet is None else alphabet
         shuffled = alphabet.copy()
         rng.shuffle(shuffled)
-        self.substitution_mapping = {plaintext: ciphertext for plaintext, ciphertext in zip(alphabet, shuffled)}
-        self.divisor = divisor
+        self.substitution_mapping = {plaintext: ciphertext for plaintext, ciphertext in zip(alphabet, shuffled)} 
+        self.mod = mod
         
     def __call__(self, m: str):
+        # Record initial state
+        initial_substitution_mapping = self.substitution_mapping.copy()
+        # Perform the encryption, shifting the cipher every mod characters
         c = ""
         for i, plaintext in enumerate(m.upper()):
-            if i // self.divisor == 0:
-                self.shift()
+            if i % self.mod == 0:
+                self.shift(i=1)
             c += self.substitution_mapping[plaintext]
+        # Make sure to reset the initial state after performing the encryption
+        self.substitution_mapping = initial_substitution_mapping
         return c
     
-    def shift(self):
+    def shift(self, i: int=1):
         keys = list(self.substitution_mapping.keys())
         values = deque(self.substitution_mapping.values())
-        values.rotate(1)
+        values.rotate(i)
         self.substitution_mapping = dict(zip(keys, values))
