0e8d1aa61ebb4247733366e57bfdf98562e23067
Tj-Alder-ANU <u7287129@anu.edu.au>
2026-01-21 21:21:26 -1000

    Fix logic bug in Caeser Shift determine_i()

--- projects/cryptography/cryptography/symmetric/caesar_shift.py
+++ projects/cryptography/cryptography/symmetric/caesar_shift.py
@@ -4,8 +4,8 @@ from collections import deque
 
 def determine_i(m: str, c: str, alphabet: List[str]=None):
     alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] if alphabet is None else alphabet
-    assert len(m)==1 and len(c)==1
-    return alphabet.index(c.upper()) - alphabet.index(m.upper())
+    assert all(char.upper() in alphabet for char in [m, c]) 
+    return alphabet.index(m.upper()) - alphabet.index(c.upper())
 
 
 def caesar_shift(m: str, i: int, alphabet: List[str]=None):
@@ -17,7 +17,7 @@ def caesar_shift(m: str, i: int, alphabet: List[str]=None):
     return c
 
 if __name__ == "__main__":
-    i = determine_i(m="i", c="o")
+    i = determine_i(m="I", c="O")
     print(i)
     m = "DAJSDA"
     c = caesar_shift(m=m, i=i)
